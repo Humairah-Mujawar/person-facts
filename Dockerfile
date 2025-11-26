bddd9024230b6e012e8cef7419c507ac4a7d2f58
Humairah <Humairah-Mujawar@users.noreply.github.com>
2025-11-26 20:57:05 +0530

    Fix for Issue #1: Not able to GET index.html
    1. Changed Dockerfile to copy 'index.html' during build
    2. Changed server.js file to redirect the root to index.html

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:20-alpine
 WORKDIR /app
 COPY server.js ./
+COPY index.html ./
 COPY .env ./
 RUN npm install express dotenv --no-save
 EXPOSE 3000
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config();
+const path = require("path")
 const app = express();
 
 app.use(function(req, res, next){
@@ -9,7 +10,9 @@ app.use(function(req, res, next){
     next()   
 })
 
-app.get('/', (req, res) => res.send('Try to go to /hello'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.get('/hello', (req, res) => {
     const n = req.query.n
